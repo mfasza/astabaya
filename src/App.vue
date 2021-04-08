e66123f729c79fe324fc3321b587e05972b3a090
Muhammad Faza <m.faza1998@gmail.com>
2021-04-08 15:58:48 +0700

    halaman awal 90%

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,96 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app dark color="#0F8ED6">
+      <div class="app-bar-logo-bg">
+        <v-img src="./assets/logo_bps.png"></v-img>
+      </div>
+      <v-toolbar-title>ASTABAYA</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main app color="#E5E5E5">
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <!-- If using vue-router -->
+        <v-slide-y-transition>
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </v-main>
+    <v-bottom-navigation
+      class="fixedMenu"
+      app
+      dark
+      v-model="menu"
+      :style="{ 'background-color': '#0F8ED6' }"
+    >
+      <v-btn value="beranda">
+        <span>Beranda</span>
+
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+
+      <v-btn value="tabel">
+        <span>Tabel</span>
+
+        <v-icon>mdi-table</v-icon>
+      </v-btn>
+
+      <v-btn value="publikasi">
+        <span>Publikasi</span>
+
+        <v-icon>mdi-book</v-icon>
+      </v-btn>
+
+      <v-menu top offset-y close-on-click close-on-content-click>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn dark v-bind="attrs" v-on="on" value="lainnya">
+            <span>Lainnya</span>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="(item, index) in items" :key="index">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    drawer: null,
+    menu: "beranda",
+    items: [
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me" },
+      { title: "Click Me 2" },
+    ],
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.app-bar-logo-bg {
+  display: flex;
+  align-items: center;
+  padding: 1px;
+  margin-right: 1rem;
+  width: 35px;
+  height: 35px;
+  background-color: hsl(177, 41%, 91%);
+  border-radius: 50%;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.fixedMenu {
+  position: fixed;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <!-- kategori container -->
+    <div class="mb-2" v-for="kategori in kategoris" :key="kategori.index">
+      <small class="text-sm-body font-weight-bold grey--text">{{
+        kategori.kategori
+      }}</small>
+      <!-- sub-kategori container -->
+      <v-slide-group
+        v-model="model"
+        class="pa-0 ma-0 d-relative"
+        active-class="success"
+      >
+        <v-slide-item
+          v-for="subKategori in kategori.subKategoris"
+          :key="subKategori.index"
+        >
+          <v-row class="ma-2" align="center" justify="center">
+            <v-scale-transition>
+              <div class="d-flex flex-column flex-wrap align-center">
+                <v-btn fab dark :color="subKategori.color">
+                  <v-icon dark>{{ subKategori.icon }}</v-icon>
+                </v-btn>
+                <small class="mt-1">{{ subKategori.subKategori }}</small>
+              </div>
+            </v-scale-transition>
+          </v-row>
+        </v-slide-item>
+      </v-slide-group>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  data: () => ({
+    model: null,
+    kategoris: [
+      {
+        kategori: "Sosial dan Kependudukan",
+        subKategoris: [
+          {
+            subKategori: "Penduduk",
+            icon: "mdi-account-multiple",
+            color: "#0F8ED6",
+          },
+          {
+            subKategori: "Kemiskinan",
+            icon: "mdi-account",
+            color: "#FFC635",
+          },
+          {
+            subKategori: "Tenaga Kerja",
+            icon: "mdi-cogs",
+            color: "#C693F9",
+          },
+          {
+            subKategori: "IPM",
+            icon: "mdi-yoga",
+            color: "#3549FF",
+          },
+          {
+            subKategori: "Pemerintahan",
+            icon: "mdi-office-building",
+            color: "#53A340",
+          },
+          {
+            subKategori: "Lainnya",
+            icon: "mdi-drag",
+            color: "grey",
+          },
+        ],
+      },
+      {
+        kategori: "Ekonomi dan Perdagangan",
+        subKategoris: [
+          {
+            subKategori: "Penduduk",
+            icon: "mdi-account-multiple",
+            color: "#0F8ED6",
+          },
+          {
+            subKategori: "Kemiskinan",
+            icon: "mdi-account",
+            color: "#FFC635",
+          },
+          {
+            subKategori: "Tenaga Kerja",
+            icon: "mdi-cogs",
+            color: "#C693F9",
+          },
+          {
+            subKategori: "IPM",
+            icon: "mdi-yoga",
+            color: "#3549FF",
+          },
+          {
+            subKategori: "Pemerintahan",
+            icon: "mdi-office-building",
+            color: "#53A340",
+          },
+          {
+            subKategori: "Lainnya",
+            icon: "mdi-drag",
+            color: "grey",
+          },
+        ],
+      },
+    ],
+  }),
 };
 </script>
