8b9f52ea2ec7731e7c4d04ab5c1da7d4b2373837
Mukhammad Kharis <081332210xxx@gmail.com>
2021-05-09 14:24:48 +0700

    tambah konsep di subkategori

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,60 +1,12 @@
 <template>
   <div>
-    <!-- kategori container -->
-    <!-- <div class="mb-2" v-for="kategori in kategoris" :key="kategori.id">
-      <div class="mb-2 d-flex align-center">
-        <small class="text-sm-body font-weight-bold grey--text">{{
-          kategori.kategori
-        }}</small>
-        <v-spacer></v-spacer>
-        <v-btn x-small text class="blue--text" :to="`/kategori/` + kategori.id">
-          Lainnya <v-icon>mdi-chevron-right</v-icon>
-        </v-btn>
-      </div> -->
-    <!-- sub-kategori container -->
-    <!-- <v-slide-group
-        v-model="subkategori_selected"
-        class="pa-0 ma-0"
-        active-class="success"
-      >
-        <v-slide-item
-          v-for="subKategori in kategori.subKategoris"
-          :key="subKategori.id"
-        >
-          <v-row class="ma-2" align="center" justify="center">
-            <v-scale-transition>
-              <div class="d-flex flex-column flex-wrap align-center">
-                <v-btn
-                  fab
-                  dark
-                  :color="subKategori.color"
-                  :to="
-                    `/kategori/` +
-                    kategori.id +
-                    `/subkategori/` +
-                    subKategori.id
-                  "
-                >
-                  <v-icon dark>{{ subKategori.icon }}</v-icon>
-                </v-btn>
-                <small
-                  class="mt-1 d-inline-block text-truncate text-center"
-                  style="max-width: 80px"
-                  >{{ subKategori.subKategori }}</small
-                >
-              </div>
-            </v-scale-transition>
-          </v-row>
-        </v-slide-item>
-      </v-slide-group>
-    </div> -->
-
     <!-- data strategis container -->
     <v-expansion-panels multiple>
       <v-expansion-panel 
         class="outline mb-3"
         v-for="db in dataBeranda"
         :key="db.id"
+        :readonly="db.readonly"
       >
         <v-expansion-panel-header>
           <div style="display: block">
@@ -128,100 +80,16 @@ export default {
     subkategori_selected: null,
     infografis_selected: null,
     dataBeranda: [],
-    kategoris: [],
     infografiss: [],
   }),
   created: async function () {
-    // const sub_kategori_options = [
-    //   {
-    //     kategori: "Sosial dan Kependudukan",
-    //     subKategori: [
-    //       "Indeks Pembangunan Manusia",
-    //       "Kemiskinan dan Ketimpangan",
-    //       "Kependudukan",
-    //       "Pemerintahan",
-    //       "Tenaga Kerja",
-    //     ],
-    //     icon: [
-    //       "mdi-eye",
-    //       "mdi-account",
-    //       "mdi-human-male-female",
-    //       "mdi-office-building",
-    //       "mdi-cogs",
-    //     ],
-    //     color: ["#ab47bc", "#ffc107", "#1976d2", "#283593", "#795548"],
-    //   },
-    //   {
-    //     kategori: "Ekonomi dan Perdagangan",
-    //     subKategori: [
-    //       "Industri",
-    //       "Inflasi",
-    //       "Pariwisata",
-    //       "Produk Domestik Regional Bruto",
-    //       "Transportasi",
-    //     ],
-    //     icon: [
-    //       "mdi-factory",
-    //       "mdi-currency-usd",
-    //       "mdi-map-marker",
-    //       "mdi-shopping",
-    //       "mdi-train-car",
-    //     ],
-    //     color: ["#ff6f00", "#bf360c", "#26c6da", "#37474f", "#689f38"],
-    //   },
-    //   {
-    //     kategori: "Pertanian dan Pertambangan",
-    //     subKategori: [
-    //       "Hortikultura",
-    //       "Perikanan",
-    //       "Perkebunan",
-    //       "Peternakan",
-    //       "Tanaman Pangan",
-    //     ],
-    //     icon: [
-    //       "mdi-fruit-cherries",
-    //       "mdi-fish",
-    //       "mdi-shovel",
-    //       "mdi-cow",
-    //       "mdi-sprout",
-    //     ],
-    //     color: ["grey", "red", "blue", "green", "yellow"],
-    //   },
-    // ];
-    // const kategoris = await this.fetchKategori();
-    // for (let i = 0; i < kategoris.length; i++) {
-    //   for (let j = 0; j < sub_kategori_options.length; j++) {
-    //     if (kategoris[i].kategori == sub_kategori_options[j].kategori) {
-    //       var subKategori = await this.fetchSubKategori(kategoris[i].id);
-    //       subKategori = subKategori.map(function (sk) {
-    //         return {
-    //           subKategori: sk.sub_kategori,
-    //           id: sk.id,
-    //           link: sk.link,
-    //         };
-    //       });
-    //       subKategori = subKategori.filter(function (sk) {
-    //         let r = false;
-    //         for (
-    //           let ski = 0;
-    //           ski < sub_kategori_options[j].subKategori.length;
-    //           ski++
-    //         ) {
-    //           if (sk.subKategori == sub_kategori_options[j].subKategori[ski]) {
-    //             sk.icon = sub_kategori_options[j].icon[ski];
-    //             sk.color = sub_kategori_options[j].color[ski];
-    //             r = true;
-    //           }
-    //         }
-    //         return r;
-    //       });
-    //       kategoris[i].subKategoris = subKategori;
-    //       this.kategoris.push(kategoris[i]);
-    //       break;
-    //     }
-    //   }
-    // }
-    const dataBeranda = await this.fetchDataBeranda();
+    var dataBeranda = await this.fetchDataBeranda();
+    dataBeranda = dataBeranda.map(function (db) {
+      return {
+        ...db,
+        readonly: (db.deskripsi ? false : true),
+      };
+    });
     this.dataBeranda = dataBeranda;
     var infografiss = await this.fetchInfografis();
     infografiss.sort(function (a, b) {
@@ -230,29 +98,7 @@ export default {
     infografiss = infografiss.slice(0, 5);
     this.infografiss = infografiss;
   },
-  beforeDestroy: function () {
-    document.removeEventListener("backbutton", this.yourCallBackFunction);
-  },
   methods: {
-    back: function () {
-      alert("aaa");
-    },
-    // fetchKategori: async function () {
-    //   const result = await axios
-    //     .get(this.url + "/kategori/get")
-    //     .then(function (response) {
-    //       return response.data.data.kategori;
-    //     });
-    //   return result;
-    // },
-    // fetchSubKategori: async function (kategori_id) {
-    //   const result = await axios
-    //     .get(this.url + "/sub_kategori/get?kategori_id=" + kategori_id)
-    //     .then(function (response) {
-    //       return response.data.data.sub_kategori;
-    //     });
-    //   return result;
-    // },
     fetchDataBeranda: async function () {
       const result = await axios
         .get(this.url + "/beranda/get")
--- src/views/SubKategori.vue
+++ src/views/SubKategori.vue
@@ -1,26 +1,36 @@
 <template>
   <div>
-    <v-list two-line>
-      <v-list-item-group>
-        <div v-for="tabel in tabels" :key="tabel.id">
-          <v-list-item
-            :to="
-              `/kategori/` +
-              id_kategori +
-              `/subkategori/` +
-              id_sub_kategori +
-              `/tabel/` +
-              tabel.id
-            "
-          >
-            <v-list-item-content>
-              <span class="text-subtitle-2" v-text="tabel.tabel"></span>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider></v-divider>
-        </div>
-      </v-list-item-group>
-    </v-list>
+    <v-tabs v-model="tab">
+      <v-tabs-slider color="primary"></v-tabs-slider>
+      <v-tab>Konsep</v-tab>
+      <v-tab>Tabel</v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-html="konsep"></v-tab-item>
+      <v-tab-item>
+        <v-list two-line>
+          <v-list-item-group>
+            <div v-for="tabel in tabels" :key="tabel.id">
+              <v-list-item
+                :to="
+                  `/kategori/` +
+                  id_kategori +
+                  `/subkategori/` +
+                  id_sub_kategori +
+                  `/tabel/` +
+                  tabel.id
+                "
+              >
+                <v-list-item-content>
+                  <span class="text-subtitle-2" v-text="tabel.tabel"></span>
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider></v-divider>
+            </div>
+          </v-list-item-group>
+        </v-list>
+      </v-tab-item>
+    </v-tabs-items>
   </div>
 </template>
 
@@ -30,6 +40,8 @@ import axios from "axios";
 export default {
   name: "SubKategori",
   data: () => ({
+    tab: null,
+    konsep: "",
     tabels: [],
   }),
   computed: {
@@ -41,9 +53,24 @@ export default {
     },
   },
   created: async function () {
+    var subKategoris = await this.fetchSubKategori(this.id_kategori);
+    const id_sub_kategori = this.id_sub_kategori;
+    subKategoris = subKategoris.filter(function (sk) {
+      return sk.id == id_sub_kategori;
+    });
+    this.konsep = subKategoris[0].konsep;
+    console.log(this.konsep);
     this.tabels = await this.fetchTabel(this.id_sub_kategori);
   },
   methods: {
+    fetchSubKategori: async function (kategori_id) {
+      const result = await axios
+        .get(this.url + "/sub_kategori/get?kategori_id=" + kategori_id)
+        .then(function (response) {
+          return response.data.data.sub_kategori;
+        });
+      return result;
+    },
     fetchTabel: async function (sub_kategori_id) {
       const result = await axios
         .get(this.url + "/tabel/get?sub_kategori_id=" + sub_kategori_id)
